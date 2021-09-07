2359faf28f63cfb8887fe3f237739e8c7f9b7d2f
Luke Taylor <99ltaylor@gmail.com>
2021-09-07 17:00:26 +0100

    changed get to post

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,12 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 
+post "/cat-names" do
+  p params
+  @name = params[:name]
+  erb :index
+end
+
 get "/" do
   "hello! we can reload"
 end
@@ -15,11 +21,5 @@ get "/random-cat" do
 end
 
 get "/cat-form" do
-  erb(:cat_form)
-end
-
-post "/named-cat" do
-  p params
-  @name = params[:name]
-  erb(:index)
+  erb :cat_form
 end
